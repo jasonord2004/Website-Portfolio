c881b19a8f2d3e0d085e096323217fd0d0d71e26
jasonord <jason.ord2004@gmail.com>
2024-08-20 00:53:36 -0400

    *ADDED* Scroll-up Button
    Added a scroll-up button to the website portfolio.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,6 @@
-// Nav hamburgerburger selections
-const burger = document.querySelector("#burger-menu");
-const ul = document.querySelector("nav ul");
-const nav = document.querySelector("nav");
-
-// Scroll to top selection
+//scroll to top function
 const scrollUp = document.querySelector("#scroll-up");
 
-// Select nav links
-const navLink = document.querySelectorAll(".nav-link");
-
-// Hamburger menu function
-burger.addEventListener("click", () => {
-  ul.classList.toggle("show");
-});
-
-// Close hamburger menu when a link is clicked
-navLink.forEach((link) =>
-  link.addEventListener("click", () => {
-    ul.classList.remove("show");
-  })
-);
-
-// scroll to top functionality
 scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth"});
 });
